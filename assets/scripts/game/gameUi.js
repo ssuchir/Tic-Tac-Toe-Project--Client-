65402ba1d1d69ba5dedf28cfaeba41c29b3274cc
ssuchir <ssuchir@gmail.com>
2019-10-01 08:52:12 -0400

    Updated some styling

--- assets/scripts/game/gameUi.js
+++ assets/scripts/game/gameUi.js
@@ -1,17 +1,17 @@
-'user strict'
-const store = require('../store')
-
-const ui = require('../auth/ui.js')
-
-const gameBoard = ['', '', '', '', '', '', '', '', '']
-
-// keep track of player
-let play = 'x'
-// swap player
-const swapPlay = function() {
-  if (play === 'x') {
-    play = 'o'
-  } else {
-    play = 'x'
-  }
-}
+// 'user strict'
+// const store = require('../store')
+//
+// const ui = require('../auth/ui.js')
+//
+// const gameBoard = ['', '', '', '', '', '', '', '', '']
+//
+// // keep track of player
+// let play = 'x'
+// // swap player
+// const swapPlay = function() {
+//   if (play === 'x') {
+//     play = 'o'
+//   } else {
+//     play = 'x'
+//   }
+// }
--- assets/scripts/game/gamelogic.js
+++ assets/scripts/game/gamelogic.js
@@ -5,28 +5,48 @@ const ui = require('./gameUi.js')
 let player = 'x'
 
 const switchPlayer = function () {
-  if (player === "x") {
-    player = "y"
+  if (player === 'x') {
+    player = 'o'
   } else {
-    player = "x"
+    player = 'x'
   }
 }
 
 const checkIfEmpty = function (box) {
-  if (($(box).html() === 'x') || ($(box).html() === 'y')) {
+  if (($(box).html() === 'x') || ($(box).html() === 'o')) {
     console.log('click an empty space')
   } else {
+
     $(box).html(player)
+    console.log('Box ID is', $(box).attr('id'))
+   const position = $(box).attr('id')
+
+   // gameBoard[position] = player
+
+
+   // if (gameBoard[0] !== '' && gameBoard[0] === gameBoard[1] && gameBoard[1] === gameBoard[2]) {
+   //   console.log('WINNER')
+   // } else if (gameBoard[3] !== '' && gameBoard[3] === gameBoard[4] && gameBoard[4] === gameBoard[5]) {
+   //   console.log('WINNER')
+   // } else if (gameBoard[6] !== '' && gameBoard[6] === gameBoard[7] && gameBoard[7] === gameBoard[8]) {
+   //   console.log('WINNER')
+   // } else if (gameBoard[0] !== '' && gameBoard[0] === gameBoard[3] && gameBoard[3] === gameBoard[6]) {
+   //   console.log('WINNER')
+   // } else if (gameBoard[1] !== '' && gameBoard[1] === gameBoard[4] && gameBoard[4] === gameBoard[7]) {
+   //   console.log('WINNER')
+   // } else if (gameBoard[2] !== '' && gameBoard[2] === gameBoard[5] && gameBoard[5] === gameBoard[8]) {
+   //   console.log('WINNER')
+   // } else if (gameBoard[0] !== '' && gameBoard[0] === gameBoard[4] && gameBoard[4] === gameBoard[8]) {
+   //   console.log('WINNER')
+   // } else if (gameBoard[2] !== '' && gameBoard[2] === gameBoard[4] && gameBoard[4] === gameBoard[6]) {
+   //   console.log('WINNER')
     switchPlayer()
   }
 }
 
 const playGame = function (event) {
-  // console.log(event.target)
-  // console.log(event.target.dataset.box)
   const box = event.target
   checkIfEmpty(box)
-  // $(box).html(player)
 }
 
 module.exports = {
